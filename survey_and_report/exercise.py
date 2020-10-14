c9ffa165c1f768b65bfb0dedc28e925c11e9e410
Mirabbos <willparkerboss@gmail.com>
2020-10-14 23:17:21 +0300

    "Project ex: updates"

--- survey_and_report/exercise.py
+++ survey_and_report/exercise.py
@@ -12,13 +12,15 @@ questions_answers: Dict = {
     },
     'Q5_answer': {
         "choice": "Yes",
-        "yes_choices": ["Applying Aloe liquid", "Applying lemon", "Applying garlic water"],
+        "yes_choices": ["Applying Aloe liquid", "Applying lemon",
+                        "Applying garlic water", "Do not washing"]
     },
 }
 
 # Here you receive user's answers and you have to figure out what kind of report text to return
 report_text = ''
-#
+
+
 # if questions_answers["Q1_answer"] == "Very short":
 #     report_text += "Lorem ipsum 1. Question - ANSWER_1 dolor sit amet," \
 #                 "consectetur adipiscing elit. Mauris sed ligula vitae tellus" \
@@ -95,7 +97,7 @@ report_text = ''
 #
 # # print(report_text)
 #
-# # Third question report te xt
+# # Third question report text
 # if "Psoriasis" in questions_answers["Q3_answer"] and\
 #         "Dry hair" in questions_answers["Q3_answer"] and\
 #         "Dandruff" in questions_answers["Q3_answer"]:
@@ -151,26 +153,119 @@ report_text = ''
 #                    "cillum dolore eu fugiat nulla pariatur. Excepteur" \
 #                    "sint occaecat cupidatat non proident, sunt in culpa" \
 #                    "qui officia deserunt mollit anim id est laborum."
+#
+# # Fourth question report text
+#
+# Q4: Dict = questions_answers["Q4_answer"]
+#
+# if (
+#     Q4["price"] > 120 and Q4["currency"] == "Eur" or
+#     Q4["price"] > 265 and Q4["currency"] == "USD"
+# ):
+#     calculation_1: int = round(Q4["price"] * 1.3)
+#     report_text += "Lorem ipsum dolor sit amet, consectetur adipiscing elit." \
+#                    f"Calories: {calculation_1}" \
+#                    "Vivamus hendrerit arcu eros, nec bibendum mi" \
+#                    "sodales id. Ut auctor nisl a placerat porttitor." \
+#                    "Duis at tortor posuere, gravida sapien in," \
+#                    "fermentum ligula. Quisque eu ipsum lobortis, hendrerit" \
+#                    "justo vitae, varius nisi. Etiam in leo feugiat purus" \
+#                    "facilisis tempor. Fusce congue metus non massa mollis," \
+#                    "id imperdiet ex viverra." \
+#                    f"Cras Calories: {calculation_1}" \
+#                    "imperdiet lectus at imperdiet ornare."
+#
+# elif (
+#     Q4["price"] < 60 and Q4["currency"] == "Eur" or
+#     Q4["price"] < 132 and Q4["currency"] == "USD"
+# ):
+#     calculation_2: int = round(Q4["price"] * 1.3 * 1.1)
+#     report_text += "Lorem ipsum dolor sit amet, consectetur adipiscing elit." \
+#                    "Integer porta at odio ac rhoncus." \
+#                    f"Calories: {calculation_2}" \
+#                    "nteger viverra porta eros nec ultrices." \
+#                    "Nullam ante sem, tincidunt vitae orci id," \
+#                    "vestibulum auctor risus. Phasellus sit amet" \
+#                    "lobortis eros. Maecenas convallis dolor ex," \
+#                    "vel congue ipsum ornare eu. Nunc in mattis dolor," \
+#                    "quis posuere lorem." \
+#                    f"Calories: {calculation_2}" \
+#                    "Nullam condimentum semper diam, lacinia tempor eros" \
+#                    "tristique ut. Etiam ultrices imperdiet tortor at" \
+#                    "eleifend. Aenean lorem felis, volutpat eu euismod at," \
+#                    "congue id erat. Duis luctus quam vitae mattis tempus."
+#
+# else:
+#     calculation_3: int = round(Q4["price"] * 1.3 * 15.2)
+#     report_text += "Sed at aliquam ex. Vestibulum maximus erat in justo" \
+#                    "maximus posuere." \
+#                    f"Calories: {calculation_3}" \
+#                    "Suspendisse tellus magna, faucibus scelerisque dapibus et," \
+#                    "luctus egestas nibh. Pellentesque eleifend mauris ac" \
+#                    "volutpat ullamcorper. Aenean vitae velit et nulla egestas" \
+#                    "viverra sit amet eu eros." \
+#                    f"Calories: {calculation_3}" \
+#                    "Nunc congue rutrum sem"
+#
+Q5: Dict = questions_answers["Q5_answer"]
 
-# Fourth question report text
-Q4: Dict = questions_answers["Q4_answer"]
+if Q5["choice"] == "I don't know":
+
+    report_text += "Phasellus ac sem ornare, ANSWER_I_DONT_KNOW euismod" \
+                   "tellus id, sagittis felis. Nullam viverra est nibh," \
+                   "et dignissim elit tincidunt nec. Integer vel dolor" \
+                   "aliquam, eleifend metus in, tincidunt erat. Nam id" \
+                   "facilisis tortor. Donec malesuada, libero nec tincidunt" \
+                   "ANSWER_I_DONT_KNOW commodo, nulla velit imperdiet" \
+                   "mauris, sit amet cursus dui quam maximus justo." \
+                   "In accumsan nisi ut orci finibus ullamcorper. Aliquam" \
+                   "consequat risus non orci dapibus, id commodo erat egestas."
+
+if Q5["choice"] == "No":
+
+    report_text += "Nam maximus et massa laoreet congue. In facilisis egestas" \
+                   "neque. Nullam ac euismod nibh. ANSWER_NO Aenean pulvinar" \
+                   "lacinia ligula, nec lobortis magna accumsan sed. Duis" \
+                   "tempor pellentesque quam. ANSWER_NO Sed non est dui." \
+                   "Sed commodo odio vel augue pellentesque, et sagittis" \
+                   "dolor tristique. Phasellus mollis magna eu egestas" \
+                   "viverra. Cras elementum erat vel libero venenatis," \
+                   "ut suscipit nibh scelerisque."
 
 if (
-    Q4["price"] > 120 and Q4["currency"] == "Eur" or
-    Q4["price"] > 265 and Q4["currency"] == "USD"
-):
+    Q5["choice"] == "Yes" and
+    ["Applying lemon"] and ["Applying garlic water"]
+    in Q5["yes_choice"]):
 
-    calculation: int = round(Q4["price"] * 1.3)
+    report_text += "Mauris viverra lobortis ante, eget faucibus felis" \
+                   "pulvinar et. Suspendisse urna diam," \
+                   "ANSWER_YES and ANSWER_YES_CHOICE_2, ANSWER_YES_CHOICE_3" \
+                   "elementum nec tincidunt ornare, convallis condimentum" \
+                   "nisi. Nam gravida ac magna eget cursus. ANSWER_YES" \
+                   "and ANSWER_YES_CHOICE_2, ANSWER_YES_CHOICE_3 Maecenas" \
+                   "fermentum lacus eu tempor condimentum. Quisque tristique" \
+                   "viverra justo, et mollis magna ornare a. In lacus elit," \
+                   "vestibulum a ex facilisis, faucibus gravida dui. Morb" \
+                   " consectetur egestas tempor. Sed neque ex, condimentum" \
+                   "congue facilisis non, aliquet sed odio."
+
+if (
+    Q5["choice"] == "Yes" and
+    ["Applying Aloe liquid"] and ["Do not washing"]
+    in Q5["yes_choice"]):
+
+    report_text += "Fusce sem est, maximus ac efficitur in, accumsan eu" \
+                   "libero. Praesent facilisis, augue at pretium malesuada," \
+                   "ANSWER_YES and ANSWER_YES_CHOICE_1, ANSWER_YES_CHOICE_4" \
+                   "erat eros eleifend velit, at iaculis nunc nisi nec odio." \
+                   "Ut consequat ac metus a bibendum. Donec venenatis" \
+                   "euismod eros ac dignissim. Donec dictum odio a augue" \
+                   "tincidunt interdum."
+
+else:
     report_text += "Lorem ipsum dolor sit amet, consectetur adipiscing elit." \
-                   f"Calories: {calculation}" \
-                   "Vivamus hendrerit arcu eros, nec bibendum mi" \
-                   "sodales id. Ut auctor nisl a placerat porttitor." \
-                   "Duis at tortor posuere, gravida sapien in," \
-                   "fermentum ligula. Quisque eu ipsum lobortis, hendrerit" \
-                   "justo vitae, varius nisi. Etiam in leo feugiat purus" \
-                   "facilisis tempor. Fusce congue metus non massa mollis," \
-                   "id imperdiet ex viverra." \
-                   f"Cras Calories: {calculation}" \
-                   "imperdiet lectus at imperdiet ornare."
-
-print(report_text)
+                   "Pellentesque sed scelerisque nulla, at mattis mauris." \
+                   "Vestibulum dignissim viverra nulla quis tempus." \
+                   "(Any other case) Donec finibus nisl sapien, sed auctor" \
+                   "elit sodales ac. Nulla dictum ante ante, eget maximus" \
+                   "mi efficitur nec."
