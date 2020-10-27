198829c81b9158529e599a502e9c3274e814f6f9
Mirabbos <willparkerboss@gmail.com>
2020-10-27 23:39:04 +0200

    "October 27 Night update"

--- report/base.py
+++ report/base.py
@@ -1,36 +1,36 @@
-from typing import List
 from survey.base import Survey
 from report.questions import (
-    Question_1,
-    Question_2,
-    Question_3,
-    Question_4,
-    Question_5,
+    Question
 )
 
 class Report:
     def __init__(self, survey: Survey):
         self.survey = survey
 
-    def get_questions(self):
+    def get_question_answers(self):
         return [
-            Question_1(self.survey.answers["Q1_answer"]),
-            Question_2(self.survey.answers["Q2_answer"]),
-            Question_3(self.survey.answers["Q3_answer"]),
-            Question_4(answer_currency="Eur", answer_price=120),
-            Question_5(
-                answer_choice="Yes",
-                answer_yes_choices=["Applying Aloe liquid",
-                                    "Applying lemon",
-                                    "Applying garlic water",
-                                    "Do not washing"]
-            )
+            Question(
+                self.survey.answers["Q1_answer"]
+            ).get_how_long_hair_do_you_have_answer(),
+            Question(
+                self.survey.answers["Q2_answer"]
+            ).get_how_often_do_you_wash_your_hair_answer(),
+            Question(
+                self.survey.answers["Q3_answer"]
+            ).get_what_kind_of_problems_do_you_have_answer(),
+            Question(
+                self.survey.answers["Q4_answer"]
+            ).get_whats_the_price_for_your_shampoo_answer(),
+            Question(
+                self.survey.answers["Q5_answer"]
+            ).get_solutions_you_can_do_at_home_answer(),
+
         ]
 
     def get(self):
         text: str = ""
-        for question in self.get_questions():
-            text += question.get_answer()
+        for answer_text in self.get_question_answers():
+            text += answer_text.get_answer()
         return text
 
 survey = Survey(
--- report/questions.py
+++ report/questions.py
@@ -1,6 +1,6 @@
 from report.texts import Text
 from typing import List
-from report.base import Survey
+
 
 class Answers:
     VERY_SHORT = "Very short"
@@ -21,8 +21,6 @@ class Answers:
     BAMBOO_HAIR = "Bamboo hair"
     VERY_OILY_HAIR = "Very oily hair"
 
-    currency = "Eur"
-    price = 120
     EUR = "Eur"
     USD = "USD"
     price_120 = 120
@@ -30,50 +28,61 @@ class Answers:
     price_60 = 60
     price_132 = 132
 
-    choice = answer_choice
-    yes_choices = answer_yes_choices
     YES = "Yes"
     IDK = "I don't know"
     NO = "No"
 
 class Question:
 
-    def __init__(self, answer):
+    def __init__(
+        self,
+        answer: str,
+        answer_choice: str = None,
+        answer_yes_choices: List = None,
+        currency: str = None,
+        price: int = None,
+
+    ):
         self.answer = answer
+        self.text = Text()
+        self.answer_choice = answer_choice
+        self.answer_yes_choices = answer_yes_choices
+        self.currency = currency
+        self.price = price
 
-        def get_how_long_hair_do_you_have_answer(self):
-            if (
-                    self.answer == Answers.VERY_SHORT
-            ):
-                return Text.ANSWER_VERY_SHORT
+    def get_how_long_hair_do_you_have_answer(self):
+        if (
+                self.answer == Answers.VERY_SHORT
+        ):
+            return self.text.ANSWER_VERY_SHORT
 
-            if (
-                    self.answer == Answers.SHORT
-            ):
-                return Text.ANSWER_SHORT
+        if (
+                self.answer == Answers.SHORT
+        ):
+            return self.text.ANSWER_SHORT
 
-            if (
-                    self.answer == Answers.MEDIUM
-            ):
-                return Text.ANSWER_MEDIUM
+        if (
+                self.answer == Answers.MEDIUM
+        ):
+            return self.text.ANSWER_MEDIUM
 
-            if (
-                    self.answer == Answers.LONG
-            ):
-                return Text.ANSWER_LONG
+        if (
+                self.answer == Answers.LONG
+        ):
+            return self.text.ANSWER_LONG
 
     def get_how_often_do_you_wash_your_hair_answer(self):
         if (
             self.answer == Answers.EVERYDAY or
             self.answer == Answers.TWO_TIMES
         ):
-            return Text.ANSWER_EVERDAY_AND_ANSWER_2_TIMES
+            return self.text.ANSWER_EVERDAY_AND_ANSWER_2_TIMES
 
         if (
             self.answer == Answers.ONE_TIME or
             self.answer == Answers.THREE_TIMES
         ):
-            return Text.ANSWER_1_TIME_AND_ANSWER_3_TIMES
+            return self.text.ANSWER_1_TIME_AND_ANSWER_3_TIMES
 
     def get_what_kind_of_problems_do_you_have_answer(self):
         if (
@@ -81,82 +90,74 @@ class Question:
             Answers.DRY_HAIR in self.answer and
             Answers.DANDRUFF in self.answer
         ):
-            return Text.QUESTION_3_ANSWER_1
+            return self.text.QUESTION_3_ANSWER_1
 
         elif (
             Answers.PSORIASIS in self.answer and
             Answers.HEAD_LICE in self.answer
         ):
-            return Text.QUESTION_3_ANSWER_2
+            return self.text.QUESTION_3_ANSWER_2
 
         elif (
             Answers.VERY_OILY_HAIR in self.answer and
             Answers.DRY_HAIR in self.answer or
             Answers.DANDRUFF in self.answer
         ):
-            return Text.QUESTION_3_ANSWER_3
+            return self.text.QUESTION_3_ANSWER_3
 
         else:
-            return Text.QUESTION_3_ANSWER_3_else
+            return self.text.QUESTION_3_ANSWER_3_else
 
     def get_whats_the_price_for_your_shampoo_answer(self):
         if (
-            Answers.price > Answers.price_120 and
-            Answers.currency == Answers.EUR or
-            Answers.price > Answers.price_265 and
-            Answers.currency == Answers.USD
+            self.price> Answers.price_120 and
+            self.currency == Answers.EUR or
+            self.price > Answers.price_265 and
+            self.currency == Answers.USD
         ):
             return Text().get_price_higher_than_120_or_265(
-                calculation=round(Answers.price * 1.3)
+                calculation=round(self.price * 1.3)
             )
 
         elif (
-            Answers.price < Answers.price_60 and
-            Answers.currency == Answers.EUR or
-            Answers.price < Answers.price_132 and
-            Answers.currency == Answers.USD
+            self.price < Answers.price_60 and
+            self.currency == Answers.EUR or
+            self.price < Answers.price_132 and
+            self.currency == Answers.USD
         ):
             return Text().get_price_lower_than_60_or_132(
-                calculation=round(Answers.price * 1.3 * 1.1)
+                calculation=round(self.price * 1.3 * 1.1)
             )
 
         else:
             return Text().get_price_any_other_case(
-                calculation=round(Answers.price * 1.3 * 15.2)
+                calculation=round(self.price * 1.3 * 15.2)
             )
-
-class Question_5:
-    def __init__(self, answer_choice: str, answer_yes_choices: List):
-        self.answer_choice = answer_choice
-        self.answer_yes_choices = answer_yes_choices
-        self.ANSWER_YES = "Yes"
-        self.ANSWER_IDK = "I don't know"
-        self.NO = "No"
 
     def get_solutions_you_can_do_at_home_answer(self):
         if (
-            self.answer_choice == self.ANSWER_IDK
+            self.answer_choice == Answers.IDK
         ):
-            return Text.ANSWER_IDK
+            return self.text.ANSWER_IDK
 
         if (
-            self.answer_choice  == self.NO
+            self.answer_choice  == Answers.NO
         ):
-            return Text.ANSWER_NO
+            return self.text.ANSWER_NO
 
         if (
-            self.answer_choice == self.ANSWER_YES and
+            self.answer_choice == Answers.YES and
             "Applying lemon" in self.answer_yes_choices and
             "Applying garlic water" in self.answer_yes_choices
         ):
-            return Text.ANSWER_YES_lemon_garlicwater
+            return self.text.ANSWER_YES_lemon_garlicwater
 
         if (
-            self.answer_choice == self.ANSWER_YES and
+            self.answer_choice == Answers.YES and
             "Applying Aloe liquid" in self.answer_yes_choices and
             "Do not washing" in self.answer_yes_choices
         ):
-            return Text.ANSWER_YES_aloe_not_washing
+            return self.text.ANSWER_YES_aloe_not_washing
 
         else:
-            return Text.ANSWER_YES_else
+            return self.text.ANSWER_YES_else
