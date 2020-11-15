5bc39d3c89ed699ca1ade97dc3955bc99522d468
Mirabbos Mirzakhmedov <willparkerboss@gmail.com>
2020-11-15 12:19:13 +0200

    3 tests

--- report/tests/test_data/test_question_3.py
+++ report/tests/test_data/test_question_3.py
@@ -1,15 +1,16 @@
 import unittest
 
+from mroom.report.questions import Question
 from mroom.report.tests.test_data.question_texts import TestTest
 
 
 class Test_Question_3(unittest.TestCase):
 
     def test_question_3_DANDRUFF(self):
-        # actual_result: str = Question(
-        #     "Dandruff"
-        # ).get_what_kind_of_problems_do_you_have_answer()
-        
+        actual_result: str = Question(
+            ["Dandruff", "Dry hair", "Psoriasis"]
+        ).get_what_kind_of_problems_do_you_have_answer()
+
         expected_result: str = TestTest.QUESTION_3_ANSWER_1
 
         self.assertEqual(actual_result, expected_result)
@@ -21,3 +22,6 @@ class Test_Question_3(unittest.TestCase):
     # HEAD_LICE = "Head lice"
     # BAMBOO_HAIR = "Bamboo hair"
     # VERY_OILY_HAIR = "Very oily hair"
+
+
+s
