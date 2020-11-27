976870e74bb59dc60768835be7f55bd51fc093e8
Mirabbos Mirzakhmedov <willparkerboss@gmail.com>
2020-11-27 02:51:05 +0200

    "November 27 updates"

---
+++ products/base.py
@@ -0,0 +1,11 @@
+#TODO: Exercise 1: A proper request following all the previous instructions
+# 1. Add variable type to the 'response'
+# 2. Instead of using separate 'url' variable,
+#    move 'https://amazon-products1.p.rapidapi.com/search' in .get(url=...)
+# 3. Instead of using separate 'headers' variable,
+#    move {... headers_content ...} in .get(headers=...)
+# 4. Instead of using separate 'querystring' variable,
+#    move {... querystring_content ...} in .get(params=...)
+# 5. Choose one either single or double quotes,
+#    and stick with them - replace all of them with one type.
+
