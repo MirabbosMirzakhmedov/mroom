842d8022e198c9781469544509926ccbac3963b7
Mirabbos Mirzakhmedov <willparkerboss@gmail.com>
2020-11-29 00:52:08 +0200

    improvements in products

--- products/base.py
+++ products/base.py
@@ -9,3 +9,17 @@
 # 5. Choose one either single or double quotes,
 #    and stick with them - replace all of them with one type.
 
+import requests
+
+response: requests.models.Response = requests.get(
+    url="https://amazon-products1.p.rapidapi.com/search",
+    headers={
+        'x-rapidapi-key': "a8f0a072d2msh3d1bfc104318241p1aec10jsn70718bbf81a2",
+        'x-rapidapi-host': "amazon-products1.p.rapidapi.com"
+    },
+    params={
+        "country":"US","query":"MacBook+Pro"
+    }
+)
+
+print(response.text)
