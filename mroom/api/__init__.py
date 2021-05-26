4d76726e5cf7a1966ec7baa73faf657b6e3e6136
Mirabbos Mirzakhmedov <willparkerboss@gmail.com>
2021-05-27 01:22:33 +0300

    Prepare django project

---
+++ mroom/api/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'mroom.api.apps.ApiConfig'
--- mroom/settings.py
+++ mroom/settings.py
@@ -11,25 +11,36 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+
+    # 3 rd party
+    'corsheaders',
+    'rest_framework', 
+    'django_extensions',
+
+    # Projects
+    'mroom.api'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
 ROOT_URLCONF = 'mroom.urls'
 
 TEMPLATES = [
